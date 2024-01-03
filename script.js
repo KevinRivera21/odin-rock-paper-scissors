7f7e200ae22449f4da8a6445466a4b037514c991
Kevin <kevinriverag2021@gmail.com>
2024-01-02 23:12:56 -0400

    Create round function

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let options = ["Rock", "Paper", "Scissors"];
+let options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * options.length);
     return options[computerChoice];
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock";
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats Paper"
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beats Paper";
+    }
+}
